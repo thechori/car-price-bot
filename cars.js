8ab6aad5b6209568ce3a31aec9d0c61eadc31734
Ryan Teodoro <ryanteodoro@theagilehub.net>
2019-05-07 17:23:54 -0500

    Remove cars.js

--- cars.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    filename: "1992-sc300-1jz.txt",
-    description: "1992 Toyota SC300 Single Turbo",
-    url:
-      "https://houston.craigslist.org/pts/d/league-city-92-sc300-turbo/6872004733.html",
-    desiredPrice: "$13000"
-  }
-];
