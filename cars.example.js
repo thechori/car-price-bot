1575e7e4e8aeae0156414602393e64ce1db1d4a8
Ryan Teodoro <ryanteodoro@theagilehub.net>
2019-05-07 17:31:02 -0500

    Add second car to example file

--- cars.example.js
+++ cars.example.js
@@ -4,5 +4,11 @@ module.exports = [
     description: "1994 Toyota Supra",
     url: "https://houston.craigslist.org/some-awesome-supra.html",
     desiredPrice: "$13000"
+  },
+  {
+    filename: "2008-nissan-350z-white.txt",
+    description: "2008 Nissan 350z (White)",
+    url: "https://houston.craigslist.org/cto/d/houston-z/6870467313.html",
+    desiredPrice: "$7000"
   }
 ];
